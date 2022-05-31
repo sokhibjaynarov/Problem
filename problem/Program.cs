d9e48462eab7d1a991f55e315ef9762b77791721
Sohib <jaynarovsohib@gmail.com>
2022-05-31 11:17:59 +0500

    Add project files.

---
+++ problem/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = int.Parse(Console.ReadLine());
+            int[] t = new int[10];
+            t = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] n = new int[a];
+            for(int i = 0; i < a; i++)
+            {
+                n[i] = int.Parse(Console.ReadLine());
+            }
+
+            
+        }
+
+        public static bool IsMatch(string s1, string s2)
+        {
+            char num = 'n';
+            for (int i = 0; i < 10; i++)
+            {
+                if (s1[i] != s2[i])
+                {
+                    if (num == 'n')
+                    {
+                        num = s1[i];
+                    }
+                    else if (num != s2[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
